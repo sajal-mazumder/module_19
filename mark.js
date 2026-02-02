c8e8bdd4f4fcb5384e2221fb87e1455fcaae9c1e
sajal-mazumder <sajalmazumder47@gmail.com>
2026-02-02 21:14:59 +0600

    some update

--- mark.js
+++ mark.js
@@ -1,4 +1,63 @@
 
 
-let a = 2;
-console.log(a);
+// let disha = 56;
+// let sohan = 67;
+
+// if (disha > sohan) {
+//     console.log('disha will eat strawberry');
+// }
+// else{
+//     console.log('sohan will eat strawberry');
+// }
+
+
+// using function : comparison between 2
+
+// function max (num1, num2){
+//     if (num1 > num2) {
+//         return 'disha will get strawberry';
+//     }
+//     else{
+//         return 'sohan will get strawberry';
+//     }
+// }
+ 
+// let max1 = max(45, 56);  // sohan
+// let max2 = max(87, 78); // disha
+// let finalMax = max(max1, max2); // disha
+// console.log(finalMax);
+// // console.log(max1 , max2);
+
+
+
+// comparison among 3
+
+// let shanta = 47;
+// let borsa = 45;
+// let sajal = 56;
+
+// if (shanta > borsa && shanta > sajal) {
+//     console.log('shanta will get gift.')
+// }else if (borsa> shanta && borsa > sajal) {
+//     console.log('borsa will get the gift')
+// }
+// else{
+//     console.log('sajal will get the gift')
+// }
+
+// using function
+
+function getMax(n1, n2, n3){
+    if (n1> n2 && n1 > n3) {
+        return 'shanta will get gift.'
+    }
+    else if (n2 > n1 && n2 > n3) {
+        return 'borsa will get the gift.'
+    }
+    else{
+        return 'sajal will get the gift'
+    }
+}
+
+ let result = getMax(67,78, 54);
+ console.log(result);
---
+++ tallest.js
@@ -0,0 +1,32 @@
+
+// find the larger number in an array
+
+let arr = [34, 4, 33, 32, 13, 25, 46, 68, 54, 47, 63];
+
+// function max(numbers){
+//     // console.log(numbers); // access the array
+//     let max = numbers[0];
+//     for(number of numbers){
+//         // console.log(number); // access the elements separately
+//         if (number > max ) {
+//             max = number;
+//         }
+//     }
+//     return max;
+// }
+// let result = max(arr);
+// console.log('result of the array max is : ', result)
+
+function getMax(numbers){
+    let maxNumber = numbers[0];
+    for(let number of numbers){
+        // console.log(maxNumber); // access all elements
+        if (number > maxNumber) {
+            maxNumber = number;
+        }
+    }
+    return maxNumber;
+}
+
+let res = getMax(arr);
+console.log('result of the array is the larger number : ', res)
